d566e659f4dfc4987da0ae7861875eb49db2f51e
Ömer Ceyhan <omer.ceyhan@partner.turktelekom.com.tr>
2020-06-11 20:47:55 +0300

    Added Ticket dto and resource fields

--- src/main/java/com/airline/ticketservice/dto/TicketDto.java
+++ src/main/java/com/airline/ticketservice/dto/TicketDto.java
@@ -3,6 +3,20 @@ package com.airline.ticketservice.dto;
 import com.airline.ticketservice.base.dto.AbstractDto;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class TicketDto extends AbstractDto {
+
+    private String ownerName;
+
+    private String ownerSurname;
+
+    private Integer ticketNumber;
+
+    private String creditCartNumber;
+
+    private BigDecimal ticketPrice;
+
+    private FlightDto flight;
 }
--- src/main/java/com/airline/ticketservice/resource/TicketResource.java
+++ src/main/java/com/airline/ticketservice/resource/TicketResource.java
@@ -3,6 +3,20 @@ package com.airline.ticketservice.resource;
 import com.airline.ticketservice.base.resource.AbstractResource;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class TicketResource extends AbstractResource {
+
+    private String ownerName;
+
+    private String ownerSurname;
+
+    private Integer ticketNumber;
+
+    private String creditCartNumber;
+
+    private BigDecimal ticketPrice;
+
+    private FlightResource flight;
 }
