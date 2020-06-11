e9dfb6d58e41ae12e41806a80efba154635f4ee4
Ömer Ceyhan <omer.ceyhan@partner.turktelekom.com.tr>
2020-06-11 20:14:12 +0300

    Added name property to AirfieldDto and AirfieldResource

--- src/main/java/com/airline/ticketservice/dto/AirfieldDto.java
+++ src/main/java/com/airline/ticketservice/dto/AirfieldDto.java
@@ -5,4 +5,5 @@ import lombok.Data;
 
 @Data
 public class AirfieldDto extends AbstractDto {
+    private String name;
 }
--- src/main/java/com/airline/ticketservice/resource/AirfieldResource.java
+++ src/main/java/com/airline/ticketservice/resource/AirfieldResource.java
@@ -5,4 +5,5 @@ import lombok.Data;
 
 @Data
 public class AirfieldResource extends AbstractResource {
+    private String name;
 }
