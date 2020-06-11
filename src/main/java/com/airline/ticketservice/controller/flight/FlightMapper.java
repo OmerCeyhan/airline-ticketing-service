e37bc702f939b0f79a2f0477995d6d696ed086f6
Ömer Ceyhan <omer.ceyhan@partner.turktelekom.com.tr>
2020-06-11 20:42:28 +0300

    Fixed inheritance of mapping classes

--- src/main/java/com/airline/ticketservice/controller/flight/FlightMapper.java
+++ src/main/java/com/airline/ticketservice/controller/flight/FlightMapper.java
@@ -1,13 +1,13 @@
 package com.airline.ticketservice.controller.flight;
 
 import com.airline.ticketservice.base.mapper.Converter;
-import com.airline.ticketservice.data.company.Company;
+import com.airline.ticketservice.controller.company.CompanyMapper;
+import com.airline.ticketservice.controller.route.FlightRouteMapper;
 import com.airline.ticketservice.data.flight.Flight;
-import com.airline.ticketservice.data.route.FlightRoute;
 import com.airline.ticketservice.dto.FlightDto;
 import com.airline.ticketservice.resource.FlightResource;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring", uses = {FlightRoute.class, Company.class})
+@Mapper(componentModel = "spring", uses = {FlightRouteMapper.class, CompanyMapper.class})
 public interface FlightMapper extends Converter<FlightDto, Flight, FlightResource> {
 }
--- src/main/java/com/airline/ticketservice/controller/ticket/TicketMapper.java
+++ src/main/java/com/airline/ticketservice/controller/ticket/TicketMapper.java
@@ -1,12 +1,13 @@
 package com.airline.ticketservice.controller.ticket;
 
 import com.airline.ticketservice.base.mapper.Converter;
+import com.airline.ticketservice.controller.flight.FlightMapper;
 import com.airline.ticketservice.data.ticket.Ticket;
 import com.airline.ticketservice.dto.TicketDto;
 import com.airline.ticketservice.resource.TicketResource;
 import org.mapstruct.Mapper;
 
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",uses = {FlightMapper.class})
 public interface TicketMapper extends Converter<TicketDto, Ticket, TicketResource> {
 }
